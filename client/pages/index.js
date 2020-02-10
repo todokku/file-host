d88a1bf9f75894d555572ae5a99810fc07a11106
Daniel Gulic <danielgulic@gmail.com>
2020-02-10 17:54:18 +1100

    create next app and start writing dashboard

---
+++ client/pages/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import Head from 'next/head';
+import Nav from '../components/nav';
+
+const Home = () => (
+	<div>
+		<Head>
+			<title>Home</title>
+			<link rel="icon" href="/favicon.ico" />
+			<link rel="stylesheet" href="https://unpkg.com/awsm.css/dist/awsm.min.css" />
+		</Head>
+
+		<Nav />
+
+		<div className="hero">
+			<h1 className="title">Welcome to File Host!</h1>
+			<p className="description">
+				This is a free file hosting service with support for custom domains.
+			</p>
+
+			<div className="row">
+				<a href="/dashboard" className="card">
+					<h3>Dashboard &rarr;</h3>
+					<p>Get started in the dashboard.</p>
+				</a>
+			</div>
+		</div>
+
+		<style jsx>{`
+			.hero {
+				width: 100%;
+				color: #333;
+			}
+			.title {
+				margin: 0;
+				width: 100%;
+				padding-top: 80px;
+				line-height: 1.15;
+				font-size: 48px;
+			}
+			.title,
+			.description {
+				text-align: center;
+			}
+			.row {
+				max-width: 880px;
+				margin: 80px auto 40px;
+				display: flex;
+				flex-direction: row;
+				justify-content: space-around;
+			}
+			.card {
+				padding: 18px 18px 24px;
+				width: 220px;
+				text-align: left;
+				text-decoration: none;
+				color: #434343;
+				border: 1px solid #9b9b9b;
+			}
+			.card:hover {
+				border-color: #067df7;
+			}
+			.card h3 {
+				margin: 0;
+				color: #067df7;
+				font-size: 18px;
+			}
+			.card p {
+				margin: 0;
+				padding: 12px 0 0;
+				font-size: 13px;
+				color: #333;
+			}
+		`}</style>
+	</div>
+);
+
+export default Home;
